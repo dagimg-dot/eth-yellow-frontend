7c677749634a29f11fdec1738ab5267465706d4f
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-06-07 15:56:12 +0300

    feat: setup toast

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "@/router";
 import vuetify from "@/plugins/vuetify";
-import '@/@iconify/icons-bundle';
+import "@/@iconify/icons-bundle";
 import "./scss/main.scss";
 import { defaultApolloClient } from "@/plugins/apollo";
 import { ApolloClients } from "@vue/apollo-composable";
 import VueDOMPurifyHTML from "vue-dompurify-html";
+import Vue3Toastify from "vue3-toastify";
+import 'vue3-toastify/dist/index.css';
 
 const app = createApp(App);
 
@@ -17,5 +19,6 @@ app
     default: defaultApolloClient,
   })
   .use(vuetify)
+  .use(Vue3Toastify)
   .use(VueDOMPurifyHTML)
   .mount("#app");
