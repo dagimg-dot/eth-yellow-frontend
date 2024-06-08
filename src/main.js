9a645160416f47a0b9f3320db699219ccee71dee
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-06-09 00:43:07 +0300

    chore: setup global store using pinia

--- src/main.js
+++ src/main.js
@@ -10,11 +10,14 @@ import { ApolloClients } from "@vue/apollo-composable";
 import VueDOMPurifyHTML from "vue-dompurify-html";
 import Vue3Toastify from "vue3-toastify";
 import "vue3-toastify/dist/index.css";
+import { createPinia } from "pinia";
 
 const app = createApp(App);
+const pinia = createPinia();
 
 app
   .use(router)
+  .use(pinia)
   .provide(ApolloClients, {
     default: defaultApolloClient,
   })
