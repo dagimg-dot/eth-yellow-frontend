42f199a4a58213d44bf1c9c0cc460b339ea6898d
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-06-11 16:16:01 +0300

    refactor: remove unused imports

--- src/pages/auth/Login.vue
+++ src/pages/auth/Login.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref } from 'vue';
 import { useLogin } from '@/composables/login';
 import { useRouter } from 'vue-router';
 import { toast } from 'vue3-toastify';
