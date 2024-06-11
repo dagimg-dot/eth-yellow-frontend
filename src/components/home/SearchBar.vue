a24647f288ad06b298d4c6745e6444e236ccabc7
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-06-11 20:27:41 +0300

    feat: UI and functinality changes
    - current location coordinates
    - fully responsive

--- src/components/home/SearchBar.vue
+++ src/components/home/SearchBar.vue
@@ -1,17 +1,45 @@
 <script setup>
-import { ref } from 'vue'
+import { computed, ref, watch } from 'vue'
+import { toast } from 'vue3-toastify';
+
+const CUR_LOCATION = "Current Location"
 
 const form = ref({
     location: '',
     searchQuery: '',
 })
 
+const locationChoice = computed(() => form.value.location)
+
+const mockFetchedCityList = ref(['Lagos', 'Abuja', 'Port Harcourt', 'Kano', 'Ibadan', 'Kaduna', 'Enugu', 'Benin', 'Jos', 'Sokoto', 'Warri', 'Onitsha', 'Calabar', 'Uyo', 'Owerri', 'Aba', 'Akure', 'Makurdi', 'Minna', 'Ilorin', 'Katsina', 'Ado Ekiti', 'Bauchi', 'Yola', 'Gombe', 'Awka', 'Lafia', 'Jalingo', 'Dutse', 'Birnin Kebbi', 'Gusau', 'Yenagoa', 'Asaba', 'Umuahia', 'Oshogbo', 'Abeokuta', 'Ijebu Ode', 'Ife', 'Ilesha', 'Ondo', 'Ikare', 'Owo', 'Ijero', 'Iloro'])
+
 const mockLocations = ref([
-    'Current Location', 'Lagos', 'Abuja', 'Port Harcourt', 'Kano', 'Ibadan', 'Kaduna', 'Enugu', 'Benin', 'Jos', 'Sokoto', 'Warri', 'Onitsha', 'Calabar', 'Uyo', 'Owerri', 'Aba', 'Akure', 'Makurdi', 'Minna', 'Ilorin', 'Katsina', 'Ado Ekiti', 'Bauchi', 'Yola', 'Gombe', 'Awka', 'Lafia', 'Jalingo', 'Dutse', 'Birnin Kebbi', 'Gusau', 'Yenagoa', 'Asaba', 'Umuahia', 'Oshogbo', 'Abeokuta', 'Ijebu Ode', 'Ife', 'Ilesha', 'Ondo', 'Ikare', 'Owo', 'Ijero', 'Iloro'
+    CUR_LOCATION, ...mockFetchedCityList.value
 ])
 
 const search = () => {
     console.log(form.value)
+}
+
+watch(locationChoice, () => {
+    if (form.value.location === CUR_LOCATION) {
+        locateUser()
+    }
+})
+
+const locateUser = () => {
+    console.log('Locating user . . .')
+
+    navigator.geolocation.getCurrentPosition(
+        position => {
+            console.log(position.coords.latitude);
+            console.log(position.coords.longitude);
+            form.value.location = `${position.coords.latitude}, ${position.coords.longitude}`
+        },
+        error => {
+            toast.error('Location access denied. Please enable location access to use this feature.')
+        }
+    );
 }
 
 </script>
@@ -19,12 +47,21 @@ const search = () => {
 <template>
     <VContainer class="search-wrapper pa-6">
         <VForm class="d-flex ga-4" @submit.prevent="search">
-            <VSelect  placeholder="Where" label="Where" v-model="form.location" :items="mockLocations" dense class="w-25" prepend-inner-icon="bx-map" />
-            <VTextField v-model="form.searchQuery" label="Search" placeholder="What do you need . . ."
-                prepend-inner-icon="bx-search" dense class="w-75" />
-            <VBtn type="submit" variant="flat" density>
-                Search
-            </VBtn>
+            <VRow>
+                <VCol cols="12" md="4">
+                    <VSelect v-model="form.location" :items="mockLocations" dense prepend-inner-icon="bx-map"
+                        @click:prepend-inner="locateUser" label="Location" />
+                </VCol>
+                <VCol cols="12" md="6">
+                    <VTextField v-model="form.searchQuery" label="Search" placeholder="What do you need . . ."
+                        prepend-inner-icon="bx-search" dense />
+                </VCol>
+                <VCol cols="12" md="2" width="40">
+                    <VBtn type="submit" width="100%" height="100%" density="default" >
+                        Search
+                    </VBtn>
+                </VCol>
+            </VRow>
         </VForm>
     </VContainer>
 </template>
@@ -32,7 +69,8 @@ const search = () => {
 <style scoped>
 .search-wrapper {
     border-radius: 12px;
-    backdrop-filter: blur(10px);
+    backdrop-filter: blur(15px);
+    background-color: #62752227;
     border: 1px solid rgba(255, 255, 255, 0.1);
 }
 </style>
