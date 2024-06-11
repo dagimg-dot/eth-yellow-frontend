44c84b71684872358f6052d60b1a2ac225e0e715
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-06-12 00:58:08 +0300

    refactor: hero layout change to support other section accordingly

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,20 +4,22 @@
 <template>
 	<VApp>
 		<VMain>
-			<VLayout max-width="1400px" class="mx-auto">
-				<Navigation />
-				<HomeSection />
-			</VLayout>
+			<div class="hero">
+				<VLayout max-width="1400px" class="mx-auto">
+					<Navigation />
+					<HomeSection />
+				</VLayout>
+			</div>
 		</VMain>
 	</VApp>
 </template>
 
 <style scoped>
-.v-main {
+.hero {
 	background: linear-gradient(to top, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.3)), url('../assets/images/aa_sun_set.jpeg') no-repeat center center;
 	background-size: cover;
 	background-position: center;
-	height: 100vh;
 	width: 100vw;
+	height: 100vh;
 }
 </style>
