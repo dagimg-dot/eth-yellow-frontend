4ab046757282194eef38736a14cd9db887bb653e
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-06-12 00:57:25 +0300

    chore: formatting

--- src/components/home/HomeSection.vue
+++ src/components/home/HomeSection.vue
@@ -4,11 +4,14 @@
 <template>
     <section class="d-flex justify-center align-center font-weight-bold h-screen mx-sm-auto text-sm-center">
         <VContainer class="d-flex flex-column justify-center align-center ga-11">
-            <h1 class="font-weight-bold text-lg-h1 text-md-h1 text-sm-h2 text-h3">Your Go-To Local Business Directory</h1>
+            <h1 class="font-weight-bold text-lg-h1 text-md-h1 text-sm-h2 text-h3">
+                Your Go-To Local Business Directory
+            </h1>
             <p class="text-lg-h5 text-md-h5 text-sm-h6">Welcome to ethyellow, your ultimate local business directory. we
                 have the most
                 comprehensive and up-to-date
-                listings to help you find exactly what you need.</p>
+                listings to help you find exactly what you need.
+            </p>
             <SearchBar />
         </VContainer>
     </section>
