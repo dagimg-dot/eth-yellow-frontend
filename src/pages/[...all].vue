d2c13e9c51259f850289436d24409ce356b23437
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-06-12 23:31:46 +0300

    feat: finish 404 page

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,4 +1,25 @@
+<script setup></script>
+
 <template>
-  <div>Page not found</div>
-  <RouterLink to="/">Go to home</RouterLink>
+  <div class="misc-wrapper">
+    <ErrorHeader
+      error-title="Page Not Found ⚠️"
+      error-description="We couldn't find the page you are looking for."
+    />
+
+    <!-- 👉 Image -->
+    <div class="misc-avatar w-100 text-center">
+      <VImg
+        src="../assets/images/404.png"
+        alt="Coming Soon"
+        :max-width="500"
+        class="mx-auto"
+      />
+      <VBtn to="/" class="mt-10"> Back to Home </VBtn>
+    </div>
+  </div>
 </template>
+
+<style lang="scss">
+@use "@/scss/misc.scss" as *;
+</style>
