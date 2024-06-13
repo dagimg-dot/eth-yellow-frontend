9419839534594c730f07a60bfb6665646dd4e7c0
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-06-13 14:25:18 +0300

    feat: create layout for pages with nav

---
+++ src/layouts/withnav.vue
@@ -0,0 +1,10 @@
+<template>
+  <VApp>
+    <VMain>
+      <VLayout max-width="1400px" class="mx-auto h-screen">
+        <Navigation :isLanding="false" />
+        <RouterView />
+      </VLayout>
+    </VMain>
+  </VApp>
+</template>
