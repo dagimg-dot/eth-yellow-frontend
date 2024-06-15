131d439e0ae3146581b9bb893e131d9ddd8564b4
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-06-16 00:43:21 +0300

    chore: put logged in user info to the local storeage

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -3,17 +3,19 @@ import { ref, computed } from "vue";
 
 export const useAuthStore = defineStore("auth", () => {
   const token = ref(localStorage.getItem("session") || null);
-  const user = ref(null);
+  const user = ref(JSON.parse(localStorage.getItem("user")) || null);
   const isLoggedIn = computed(() => token.value !== null);
 
-  function login(accessToken, user) {
+  function login(accessToken, _user) {
     token.value = accessToken;
+    user.value = _user;
     localStorage.setItem("session", accessToken);
-    user.value = user;
+    localStorage.setItem("user", JSON.stringify(_user));
   }
 
   function logout() {
     localStorage.removeItem("session");
+    localStorage.removeItem("user");
     token.value = null;
     user.value = null;
   }
