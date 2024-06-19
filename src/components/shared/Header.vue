30fa986500029a62d9db8f919470e6bf699bfcfa
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-06-19 12:11:26 +0300

    refactor: create header component for listing pages

---
+++ src/components/shared/Header.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <VCol>
+    <VRow justify="space-between">
+      <div class="text-h5 text-lg-h4 font-weight-bold">All Listings</div>
+      <SortMenu />
+    </VRow>
+  </VCol>
+</template>
+
+<style scoped></style>
