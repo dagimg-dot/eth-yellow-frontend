6db8ae475f90aaf780a8dff29be82fa7e9a3dd05
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-06-20 14:06:55 +0300

    fix: export get user graphql

--- src/graphql/queries/index.js
+++ src/graphql/queries/index.js
@@ -1,5 +1,6 @@
 import GET_CATEGORIES from "./getCategories.gql";
 import GET_CITIES from "./getCities.gql";
+import GET_USER from "./getUser.gql";
 import GET_LISTINGS from "./getListings.gql";
 import GET_USER_LISTINGS from "@/graphql/queries/getUserBusiness.gql";
 import { dynamicQuery } from "./getFilteredListings";
@@ -7,6 +8,7 @@ import { dynamicQuery } from "./getFilteredListings";
 export {
   GET_CATEGORIES,
   GET_CITIES,
+  GET_USER,
   GET_LISTINGS,
   GET_USER_LISTINGS,
   dynamicQuery,
