d25f38b7a972060ab4c3e86c2641c75d81825776
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-06-20 10:50:17 +0300

    chore: create index.js file to export all operations

---
+++ src/graphql/mutations/index.js
@@ -0,0 +1,11 @@
+import CREATE_LISTING_MUTATION from "@/graphql/mutations/createListing.gql";
+import LOGIN_MUTATION from "@/graphql/mutations/login.gql";
+import SIGNUP_MUTATION from "@/graphql/mutations/signup.gql";
+import UPDATE_USER_MUTATION from "@/graphql/mutations/updateUser.gql";
+
+export {
+  CREATE_LISTING_MUTATION,
+  LOGIN_MUTATION,
+  SIGNUP_MUTATION,
+  UPDATE_USER_MUTATION,
+};
---
+++ src/graphql/queries/index.js
@@ -0,0 +1,13 @@
+import GET_CATEGORIES from "./getCategories.gql";
+import GET_CITIES from "./getCities.gql";
+import GET_LISTINGS from "./getListings.gql";
+import GET_USER_LISTINGS from "@/graphql/queries/getUserBusiness.gql";
+import { dynamicQuery } from "./getFilteredListings";
+
+export {
+  GET_CATEGORIES,
+  GET_CITIES,
+  GET_LISTINGS,
+  GET_USER_LISTINGS,
+  dynamicQuery,
+};
