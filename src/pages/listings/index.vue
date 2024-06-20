64665dd76f7a7b85e1fff21abb75e1bc40373d24
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-06-20 10:51:08 +0300

    refactor: use composables for readability

--- src/pages/listings/index.vue
+++ src/pages/listings/index.vue
@@ -1,29 +1,7 @@
 <script setup>
-import { useQuery } from "@vue/apollo-composable";
-import GET_LISTINGS from "@/graphql/queries/getListings.gql";
-import { toast } from "vue3-toastify";
-import { useFilterStore } from "@/store/modules/filterStore";
-import { storeToRefs } from "pinia";
+import { useListings } from "@/composables/useListings";
 
-const filterStore = useFilterStore();
-const { listings, loading } = storeToRefs(filterStore);
-
-// Fetch Listings
-const {
-  result: listingResult,
-  loading: listingLoading,
-  onError: onListingError,
-} = useQuery(GET_LISTINGS, null, {
-  context: {
-    authRequired: false,
-  },
-});
-
-filterStore.setListings(listingResult.value?.businesses);
-
-onListingError((error) => {
-  toast.error("Failed to fetch listings, ", error.message);
-});
+const { listings, listingLoading } = useListings();
 </script>
 
 <template>
@@ -34,7 +12,7 @@ onListingError((error) => {
       <VCol lg="4" sm="6" cols="12" v-for="listing in listings">
         <ListingCard
           :listing="listing"
-          :loading="listingLoading || loading"
+          :loading="listingLoading"
           :isAllowed="false"
         />
       </VCol>
--- src/pages/user/listings.vue
+++ src/pages/user/listings.vue
@@ -1,37 +1,7 @@
 <script setup>
-import { useQuery } from "@vue/apollo-composable";
-import GET_USER_LISTINGS from "@/graphql/queries/getUserBusiness.gql";
-import { useAuthStore } from "@/store/modules/auth";
-import { storeToRefs } from "pinia";
-import { useFilterStore } from "@/store/modules/filterStore";
-import { toast } from "vue3-toastify";
+import { useUserListings } from "@/composables/userListings";
 
-const authStore = useAuthStore();
-const filterStore = useFilterStore();
-const { listings } = storeToRefs(filterStore);
-const { user } = storeToRefs(authStore);
-
-const {
-  result: listingResult,
-  loading: listingLoading,
-  onError: onListingError,
-} = useQuery(
-  GET_USER_LISTINGS,
-  {
-    owner_id: user.value.user_id,
-  },
-  {
-    context: {
-      authRequired: true,
-    },
-  }
-);
-
-filterStore.setListings(listingResult.value?.businesses);
-
-onListingError((error) => {
-  toast.error("Failed to fetch listings, ", error.message);
-});
+const { userListings, listingLoading } = useUserListings();
 </script>
 
 <template>
@@ -39,7 +9,7 @@ onListingError((error) => {
     <Header />
     <FilterBar />
     <VRow>
-      <VCol lg="4" sm="6" cols="12" v-for="listing in listings">
+      <VCol lg="4" sm="6" cols="12" v-for="listing in userListings">
         <ListingCard
           :listing="listing"
           :loading="listingLoading"
