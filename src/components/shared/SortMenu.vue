4c720adb668e57debd1b601859a508ef89f544ae
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-06-23 09:44:17 +0300

    feat: finish sort button UI

---
+++ src/components/shared/SortMenu.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from "vue";
+
+const sortOptions = ref(["Alphabet", "Date ⬆", "Date ⬇", "Rating"]);
+const choosenSort = ref("Sort by");
+const setSortOption = (option) => {
+  choosenSort.value = option;
+};
+</script>
+
+<template>
+  <div class="d-flex align-center ga-2">
+    <span>{{ choosenSort }}</span>
+    <div
+      dot
+      location="top right"
+      offset-x="1"
+      offset-y="1"
+      color="success"
+
+    >
+      <VIcon icon="solar-sort-bold" size="32" class="cursor-pointer" />
+      <VMenu offset="14px" activator="parent" location="bottom end" width="150">
+        <VList>
+          <VListItem
+            v-for="option in sortOptions"
+            :key="option"
+            @click="setSortOption(option)"
+            :active="choosenSort === option"
+          >
+            <VListItemTitle>{{ option }}</VListItemTitle>
+          </VListItem>
+        </VList>
+      </VMenu>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
