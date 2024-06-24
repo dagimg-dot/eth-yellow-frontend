7fff3970c697d86e4862755a320e544dbcfea6e2
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-06-24 11:37:14 +0300

    fix: make overflow-auto for location section

--- src/pages/listings/[id].vue
+++ src/pages/listings/[id].vue
@@ -112,7 +112,7 @@ const tabs = [
     <VCard :ref="locRef">
       <VCardTitle class="text-h5 font-weight-bold"> Location </VCardTitle>
       <div
-        class="pa-4 overflow-scroll d-lg-flex d-md-flex ga-4 justify-space-between d-block"
+        class="pa-4 overflow-auto d-lg-flex d-md-flex ga-4 justify-space-between d-block"
       >
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d43109.46404148012!2d6.139331415419217!3d47.52221401973193!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47928c21dbead7ab%3A0x409ce34b30f0970!2s70230%20Filain!5e0!3m2!1sen!2sfr!4v1640189073328!5m2!1sen!2sfr"
