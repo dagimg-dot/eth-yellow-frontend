8669e40ff77df31cf3adf95fbafecdc67016fe63
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-06-25 16:21:53 +0300

    feat: finish features section for the home page

--- src/components/home/FeatureSection.vue
+++ src/components/home/FeatureSection.vue
@@ -1,11 +1,36 @@
 <script setup>
-
+import { features } from "@/data/features";
 </script>
 
 <template>
-    <div>
-        Featured
-    </div>
+  <v-layout max-width="1400px" class="mx-auto my-8 d-flex flex-column">
+    <VRow>
+      <VCol cols="12" class="mb-8">
+        <h2 class="text-h4 text-center font-weight-bold">Features</h2>
+      </VCol>
+    </VRow>
+
+    <VRow>
+      <VCol cols="12" md="6" lg="4" class="mb-4" v-for="feature in features">
+        <VCard outlined>
+          <VCardTitle class="text-wrap">{{ feature.title }}</VCardTitle>
+          <VCardText>
+            <ul v-for="point in feature.bulletPoints">
+              <li class="mt-2">{{ point }}</li>
+            </ul>
+          </VCardText>
+        </VCard>
+      </VCol>
+    </VRow>
+  </v-layout>
 </template>
 
-<style scoped></style>
+<style scoped>
+.v-card {
+  min-height: 210px;
+}
+
+li {
+  list-style: none;
+}
+</style>
---
+++ src/data/features.js
@@ -0,0 +1,48 @@
+export const features = [
+  {
+    title: "Comprehensive Business Listings",
+    bulletPoints: [
+      "Extensive Database: Access a wide range of businesses across various industries.",
+      "Detailed Profiles: Each listing includes essential business information.",
+      "Verified Listings: Ensuring accuracy and reliability.",
+    ],
+  },
+  {
+    title: "Verified Listings: Ensuring accuracy and reliability.",
+    bulletPoints: [
+      "Advanced Search: Find businesses by name, category, location, or keywords.",
+      "Filter Options: Refine search results with various filters.",
+      "Map Integration: View business locations on an interactive map.",
+    ],
+  },
+  {
+    title: "User Reviews and Ratings",
+    bulletPoints: [
+      "Community Feedback: Read reviews and ratings from other users.",
+      "Write Reviews: Share your experiences by leaving reviews and ratings.",
+      "Verified Reviews: All reviews are moderated for authenticity.",
+    ],
+  },
+  {
+    title: "Business Management Tools",
+    bulletPoints: [
+      "Claim Your Business: Manage your listings and keep information up-to-date.",
+      "Analytics Dashboard: Access insights on user interactions and performance.",
+      "Promotional Tools: Utilize features to attract more customers.",
+    ],
+  },
+  {
+    title: "Categories and Recommendations",
+    bulletPoints: [
+      "Diverse Categories: Explore businesses across various categories.",
+      "Personalized Recommendations: Get suggestions based on your preferences.",
+    ],
+  },
+  {
+    title: "Mobile-Friendly Design",
+    bulletPoints: [
+      "Responsive Design: Optimized for all devices.",
+      "Mobile App: Access business listings on the go.",
+    ],
+  },
+];
