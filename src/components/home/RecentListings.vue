efe19974ab31fec7dceffd7ccc826f3bf19b9111
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-07-12 07:53:46 +0300

    fix: skeleton loader for all listing related pages

--- src/components/home/RecentListings.vue
+++ src/components/home/RecentListings.vue
@@ -8,10 +8,13 @@ const { recentListings, listingLoading } = useListings({ type: "recent" });
   <section class="d-flex flex-column ga-4 w-100 my-16">
     <VRow>
       <VCol cols="12" class="mb-8">
-        <h2 class="text-h4 text-center font-weight-bold">Recent Listings</h2>
+        <h2 class="text-h4 text-center font-weight-bold text-primary">Recent Listings</h2>
       </VCol>
     </VRow>
     <VRow>
+      <VCol lg="4" sm="6" cols="12" v-show="listingLoading" v-for="item in 3">
+        <VSkeletonLoader :loading="listingLoading" type="image, article" />
+      </VCol>
       <VCol lg="4" sm="6" cols="12" v-for="listing in recentListings">
         <ListingCard
           :listing="listing"
--- src/pages/user/listings.vue
+++ src/pages/user/listings.vue
@@ -13,13 +13,16 @@ const {
   <div class="d-flex flex-column ga-4 w-100">
     <Header title="Your Listings" />
     <FilterBar :isDisabled="userListings.length == 0" />
-    <VContainer v-if="userListings.length == 0" class="text-center">
+    <VContainer v-if="userListings.length == 0 && !listingLoading" class="text-center">
       <VCardTitle>You have no registered listings</VCardTitle>
       <VBtn variant="outlined" to="/listings/add" class="w-auto mt-6"
         >Add Your Business</VBtn
       >
     </VContainer>
-    <VRow v-else>
+    <VRow>
+      <VCol lg="4" sm="6" cols="12" v-show="listingLoading" v-for="item in 3">
+        <VSkeletonLoader :loading="listingLoading" type="image, article" />
+      </VCol>
       <VCol lg="4" sm="6" cols="12" v-for="listing in userListings">
         <ListingCard
           :listing="listing"
